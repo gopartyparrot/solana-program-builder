f44d1559d34448bcb0e26215997ecec0c349c064
sssrot <sunss@tutanota.com>
2021-11-23 23:20:00 +0800

    verify buffer

--- build-verify.sh
+++ build-verify.sh
@@ -2,13 +2,25 @@
 
 set -e
 
-: "${PROGRAM_ID?Need to set PROGRAM_ID}"
 : "${PROJECT?Need to set PROJECT}"
 
 cargo build-bpf --features production --manifest-path $PROJECT/Cargo.toml
 
-solana program dump $PROGRAM_ID onchain.so
-truncate -r onchain.so /app/target/deploy/*.so
-
+echo sha256sum before truncate:
 sha256sum /app/target/deploy/*.so
-sha256sum onchain.so
+
+if [[ -z "${PROGRAM_ID}" ]]; then
+  echo PROGRAM_ID not set, will not dump onchain program
+else
+  solana program dump $PROGRAM_ID onchain.so
+  truncate -r onchain.so /app/target/deploy/*.so
+  sha256sum /app/target/deploy/*.so
+  sha256sum onchain.so
+fi
+
+if [[ -z "${BUFFER}" ]]; then
+  echo BUFFER not set, will not dump buffer
+else
+  solana program dump $BUFFER buffer.so
+  sha256sum buffer.so
+fi
